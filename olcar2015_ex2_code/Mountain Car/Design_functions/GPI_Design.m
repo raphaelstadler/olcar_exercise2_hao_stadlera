8f7b1cfd902ed8eb4b9137e3e897936001ee293e
raphaelstadler <stadlerraphael@gmx.ch>
2015-04-24 12:37:29 +0200

    Extended GPI_Design

--- olcar2015_ex2_code/Mountain Car/Design_functions/GPI_Design.m
+++ olcar2015_ex2_code/Mountain Car/Design_functions/GPI_Design.m
@@ -15,52 +15,63 @@ function Controller = GPI_Design(Task,Controller,Parameters)
 %               .V -- Value function [1 x (N_pos*N_vel)]
 %               .Policy -- Optimal control policy found
 
-
 %% Initialization
-
 %Initialize the policy
-Policy = zeros(Task.S, Task.A); %[length(Task.S) x length(Task.A)]
+Policy = zeros(size(Task.S,2), Task.A); %[length(Task.S) x length(Task.A)]
 
 % Initialize the value function
 V = zeros(Task.S) % [length(Task.S) x 1]
 
-
 while true
     %% Policy Evaluation (PE) (see script section 2.6)
-    while true
+    PE_iter = 1;
+    while PE_iter <= GPI_Params.maxIter_PE
         delta_V = 0;       
         v = V;
         V = A*V + B;    % Policy update step (see script Algorithm 1)
                         % Compare with eq 2.11 (matrix form)
         delta_V = max(delta_V, norm(v - V));
+        
+        fprintf('Iteration %d of PE.\tab Maximum change in V(x): %6.4f\n', PE_iter, delta_V);
         if delta_V < Parameters.minDelta_V
             break
         end
+        PE_iter = PE_iter + 1;
     end    
     
     %% Policy Improvment (PI) (see script section 2.7)
-    policyIsStable = ones(size(Task.S));
-    b = Policy;
-    
-    % Initialize pi_x and calculate argmax_u(..)
-    % Policy update rule
-    argmax_term = 0;
-    for a = Task.A
-        argmax_term_temp = Task.P_s_sp_a + Task.R_s_a; % TODO: argmax_u(sum_sp'(P_s_sp_a*(R_s_a + alpha * V(sp))))
-        
-        if argmax_term_temp > argmax_term
-            Policy = Controller.actionD2C(a);
+    PI_iter = 1;
+    while (PI_iter <= GPI_Params.maxIter_PI) && ~policyIsStable
+        delta_Policy = 0;
+        policyIsStable = true;
+        b = Policy;
+
+        % Initialize pi_x and calculate argmax_u(..)
+        % Policy update rule
+        argmax_term = 0;
+        for a = Task.A
+            % Integrate cost decay factor GPI_Params.alpha
+            argmax_term_temp = Task.P_s_sp_a + Task.R_s_a; % TODO: argmax_u(sum_sp'(P_s_sp_a*(R_s_a + alpha * V(sp))))
+
+            if argmax_term_temp > argmax_term
+                Policy = Controller.actionD2C(a);
+            end
         end
-    end   
-    
-    if b ~= Policy
-        policyIsStable = false;
-    end
-    
-    %% Check algorithm convergence
-    if policyIsStable
-        break
+
+        delta_Policy = norm(b - Policy);
+
+        fprintf('Iteration %d of PI.\tab Maximum change in Policy: %6.4f\n', PI_iter, delta_Policy);
+        
+        %% Check algorithm convergence
+        if delta_Policy > GPI_Params.minDelta_Policy
+            policyIsStable = false;
+        end
+        PI_iter = PI_iter + 1;
     end
 end
+
+% Store optimal control policy and value function in controller
+Controller.Policy = Policy;
+Controller.V = V;
 
 end
