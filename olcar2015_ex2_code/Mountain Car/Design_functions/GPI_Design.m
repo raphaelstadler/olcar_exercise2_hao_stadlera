f35f2fc9f304bcd6535acc037b774513dc236035
raphaelstadler <stadlerraphael@gmx.ch>
2015-05-01 16:06:20 +0200

    Succeeded to make 2a) reach the goal

--- olcar2015_ex2_code/Mountain Car/Design_functions/GPI_Design.m
+++ olcar2015_ex2_code/Mountain Car/Design_functions/GPI_Design.m
@@ -17,9 +17,11 @@ function Controller = GPI_Design(Task,Controller,Parameters)
 
 %% Initialization
 %  Initialize the policy
-%  At the beginning for all statesx every action is equally likely
-Policy = ones(length(Task.S), length(Task.A)); %[length(Task.S) x length(Task.A)]
-Policy = (1/size(Policy,2))*Policy;
+pi_x_u = ones(length(Task.S), length(Task.A)); %[length(Task.S) x length(Task.A)]
+pi_x_u = (1/size(pi_x_u,2))*pi_x_u;
+
+% Initially, always use action 1
+pi = 5*ones(length(Task.S),1);
 
 % Initialize the value function
 V = zeros(length(Task.S),1); % [length(Task.S) x 1]
@@ -28,81 +30,74 @@ while true
     %% Policy Evaluation (PE) (see script section 2.6)
     PE_iter = 1;
     while PE_iter <= Parameters.maxIter_PE
-        delta_V = zeros(size(V));
+        delta_V = 0;
         
         % Naive implementation
-        for s = Task.S
-           v = V(s);
-           
-           sum_outer = 0;
-           for a = Task.A
-                sum_P_V = Parameters.alpha * dot(Task.P_s_sp_a(s,:,a),V(:));
-                sum_outer = sum_outer + Policy(s,a) * ( Task.R_s_a(s,a) + sum_P_V );
-           end % a
-           V(s) = sum_outer;
-           
-           delta_V(s) = max(delta_V(s), norm(v - V(s)));
+        for s = Task.S  % for each state (sweep)
+            v = V(s);
+            sum_outer = 0; % summation over u
+            for a = Task.A
+                sum_P_V = Parameters.alpha * dot(Task.P_s_sp_a(s,:,pi(s)),V(:));
+                %sum_outer = sum_outer + pi_x_u(s,a) * ( Task.R_s_a(s,a) + sum_P_V ); % TODO: Maybe: Task.R_s_a(s,pi(s)) ?
+                sum_outer = sum_outer + pi_x_u(s,a) * ( Task.R_s_a(s,pi(s)) + sum_P_V ); % TODO: Maybe: Task.R_s_a(s,pi(s)) ?
+            end % a
+            V(s) = sum_outer;
+            delta_V = max(delta_V, norm(v - V(s)));
         end % s
         
-%         TODO: Replace naive implementation by matrix/vector operations
-%         Someting as follows:
-%         v = V;
-%         V = A*V + B;    % Policy update step (see script Algorithm 1)
-%                         % Compare with eq 2.11 (matrix form)
-%         delta_V = max(delta_V, norm(v - V));
-        
-        %fprintf('Iteration %i of PE.\t Maximum change in V(x): %6.4f\n', PE_iter, delta_V);
-        if max(delta_V) < Parameters.minDelta_V
+        fprintf('Iteration %i of PE.\t Maximum change in V(x): %6.4f\n', PE_iter, delta_V);
+        if delta_V < Parameters.minDelta_V
             break
         end
         
         PE_iter = PE_iter + 1;
-    end    
+    end % while (PE)  
     
     %% Policy Improvment (PI) (see script section 2.7)
     PI_iter = 1;
     while (PI_iter <= Parameters.maxIter_PI)
-        delta_Policy = zeros(size(Policy));
         policyIsStable = true;
 
         % Initialize pi_x and calculate argmax_u(..)
         % Policy update rule
-        argmax_term = 0;
 
         % Naive implementation
         for s = Task.S
-            b = Policy(s);
+            b = pi(s);
             
+            argmax_term = 0;
             max_term = -1E6;
             for a = Task.A
-                term = Task.R_s_a(s,a) + Parameters.alpha * dot(Task.P_s_sp_a(s,:,a), V);
+                term = Task.R_s_a(s,a) + Parameters.alpha * dot(Task.P_s_sp_a(s,:,a), V(:));
                 if term > max_term
                     max_term = term;
                     argmax_term = a;
                 end
             end
             
-            Policy(s) = Controller.actionD2C(argmax_term);
+            pi(s) = argmax_term;
             
-            delta_Policy(s) = norm(b - Policy(s));
-            if delta_Policy(s) > Parameters.minDelta_Policy
+            if b ~= pi(s)
                 policyIsStable = false;
             end
         end
-        
-%         TODO: Replace naive implementation by matrix/vector operations
-%         Someting as follows:        
-%         term = Task.R_s_a(s,:) + Parameters.alpha * Task.P_s_sp_a(s,:,:) * V;
-        
+         
         %fprintf('Iteration %i of PI.\t Maximum change in Policy: %6.4f\n', PI_iter, delta_Policy);
+        fprintf('Iteration %i of PI.\n',PI_iter);
         PI_iter = PI_iter + 1;
-
+        
         %% Check algorithm convergence
         if policyIsStable
             % Store optimal control policy and value function in controller
             % and return
+            
+            % Change policy dimensions from vector to matrix format
+            Policy = zeros(length(Task.S),length(Task.A));
+            for s = Task.S
+               Policy(s,pi(s)) = 1; 
+            end
             Controller.Policy = Policy;
-            Controller.V = V;
+            %Controller.V = V;
             return;
         end
     end
