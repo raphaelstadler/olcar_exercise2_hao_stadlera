a9068ffb1783bcb296869fc761eb153551508ec3
raphaelstadler <stadlerraphael@gmx.ch>
2015-05-06 17:48:54 +0200

    Succeeded to make Monte_Carlo run (somehow)

--- olcar2015_ex2_code/Cliff World/Design_functions/Monte_Carlo.m
+++ olcar2015_ex2_code/Cliff World/Design_functions/Monte_Carlo.m
@@ -19,37 +19,44 @@ function Q = Monte_Carlo(S,A,stateS2X,stateX2S,Parameters)
 
 %% Initialize Variables
 
-% Choose an arbitrary initial action value function
-Q = zeros(length(S),length(A));
+% Choose an arbitrary initial action-value function
+Q = ones(length(S),length(A));
 
-% Choose an arbitrary initial policy (index)
+% Choose an arbitrary initial policy (with action indices)
 pi_ind = ones(length(S));
 
 %Create object for incremental plotting of reward after each episode
 windowSize = 10; %Sets the width of the sliding window filter used in plotting
 plotter = RewardPlotter(windowSize);
 
-isStatePartOfEpisode = zeros(length(S));
-
 %% On-Policy Monte Carlo Algorithm (see script section 2.9.3) - Algorithm 6, p.59
 for TrainLoop = 1:Parameters.training_iterations
+    
+    % Prepare a log-variables which is used to store...
+    % ...the accumulated returns
+    Returns = zeros(length(S), length(A));
+    % ...if the state-action is part of the episode (via indicator
+    % variable)
+    isStateActionPartOfEpisode = zeros(length(S),length(A));
+    % ...the exact order the state and actions appeared in the episode
+    StateActionLog = -1*ones(2,Parameters.episode_length);
+        
     %% Generate a training episode (a)
     
     % Initialize the current state of the episode
     % Each episode has to begin at the starting point
-    currState = stateX2S([4 1]');   
-    
-    % Initialize indicator variable if a specific state is part of this episode
-    isStatePartOfEpisode(:) = 0;
-    
+    currState = stateX2S([4 1]');    
     episodicReward = 0;
     episodeSize = 1;
     isEpisodeEnd = false;
-    while (episodeSize <= Parameters.episode_length) && ~isEpisodeEnd %Episode termination criteria
-        isStatePartOfEpisode(currState) = 1;
-        
+    counter = 0;
+    while (episodeSize <= Parameters.episode_length) && ~isEpisodeEnd %Episode termination criteria    
         % Execute the current epsilon-Greedy Policy
-        action = pi_ind(currState); % A:possible action indices, pi_ind(currState): greedy action index   
+        %action = pi_ind(currState); % A:possible action indices, pi_ind(currState): greedy action index   
+        action = choose_epsilon_greedy(A, pi_ind(currState));
+        
+        isStateActionPartOfEpisode(currState,action) = 1;
+        StateActionLog(:,episodeSize) = [currState; action];
         
         % Interaction with environment
         %Note that this function takes and returns states expressed by
@@ -57,24 +64,43 @@ for TrainLoop = 1:Parameters.training_iterations
         %necessary.
         [xp,reward,isEpisodeEnd] = Cliff_World(stateS2X(currState),action);
         nextState = stateX2S(xp);
-    
-        % Policy Evaluation (b)
-        Q(currState,action) = Q(currState,action) + Parameters.omega*(reward - Q(currState,action));       
         
         % Log data for the episode
+        for iter=1:episodeSize
+            s_index = StateActionLog(1,iter);
+            a_index = StateActionLog(2,iter);
+            
+            Returns(s_index,a_index) = Returns(s_index,a_index) + reward;
+        end
+        
+        if reward ~= -1
+            %fprintf('reward: %d\n',reward);
+            counter = counter+1;
+        end
+        if isEpisodeEnd
+            fprintf('EpisodeEnd\n');
+        end
         episodicReward = episodicReward + reward;
         
         currState = nextState;
         episodeSize = episodeSize + 1;
     end
-    fprintf('Iteration %i\t Episode length:\t %d\t epsilon = %6.6f\n', TrainLoop, (episodeSize-1), Parameters.epsilon);
+    fprintf('%d times reward other than -1\n',counter);
+    fprintf('Iteration %i\t Episode length:\t %d\t epsilon = %6.6f\t episodicReward = %6.6f\n', TrainLoop, (episodeSize-1), Parameters.epsilon, episodicReward);
     
+    IndexMatrix = isStateActionPartOfEpisode==1;
+    
+    % Policy Evaluation (b)
+    % For all states and actions appearing in the episode
+    Q(IndexMatrix) = Q(IndexMatrix) + Parameters.omega*(Returns(IndexMatrix) - Q(IndexMatrix));
+   
     %% Monte Carlo Policy Improvement Step (c)
     % For each state of the episode
-    for s=S(isStatePartOfEpisode==1)
+    StateIndexVector = sum(IndexMatrix,2) > 0;
+    for s=S(StateIndexVector)
         [~,u_star] = max(Q(s,:));
         
-        pi_ind(s) = choose_epsilon_greedy(A, u_star); % A:possible action indices, u_star: greedy action index   
+        pi_ind(s) = u_star; %choose_epsilon_greedy(A, u_star); % A:possible action indices, u_star: greedy action index   
     end
     
     %Update the reward plot
